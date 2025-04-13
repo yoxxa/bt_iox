f3afd34fb3cdf72e3168c00f0aa37a139b32025a
yoxxa <theyoxxa@protonmail.com>
2025-04-13 21:00:29 +0800

    feature: designed ::collect_data() to pair gathered MAC addresses and timestamp, Vec::push() to self.data

--- src/parani.rs
+++ src/parani.rs
@@ -7,6 +7,7 @@ use serialport;
 const BT_CANCEL: &[u8; 12] = b"AT+BTCANCEL\r";
 const BT_INQ: &[u8; 10] = b"AT+BTINQ?\r";
 
+#[derive(Debug)]
 struct BtInqData {
     mac_address: String,
     timestamp: OffsetDateTime
@@ -36,5 +37,33 @@ impl ParaniSD1000 {
 
     fn bt_inq(&mut self) {
         self.port.write_all(BT_INQ).expect("BT_INQ write error");
+    }
+
+    fn collect_data(&mut self) {
+        let mut port = BufReader::new(&mut *self.port);
+        loop {
+            let mut serial_buf = String::new();
+            // TODO - Handle timeout error, will occur once .timeout() duration is reached. Use match.
+            port.read_line(&mut serial_buf).expect("Error reading");
+    
+            // TODO - Use match statements in future iteration?
+            if serial_buf != "OK\r\n" {
+                if serial_buf != "\r\n"
+                && serial_buf != "\\r\\n"
+                && serial_buf != ""
+                && serial_buf != "ERROR\\r\\n"
+                && serial_buf != "ERROR\r\n" {
+                    let parts = serial_buf.split(",");
+                    let collection: Vec<&str> = parts.collect();
+                    
+                    self.data.push(BtInqData {
+                        mac_address: collection[0].to_string(),
+                        timestamp: OffsetDateTime::now_utc()
+                    });
+                }
+            } else {
+                break;
+            }
+        }
     }
 }
