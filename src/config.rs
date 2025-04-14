f7429c3e2b954839e3875bde3f99bfe83b2d4d55
yoxxa <theyoxxa@protonmail.com>
2025-04-14 17:02:32 +0800

    feature: add [parani][asset_number] .ini config file field

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 use configparser::ini::Ini;
 
 pub struct Configuration {
-    server_ip_address: String,
-    server_port: u64,
-    logging_type: String,
-    syslog_server_ip_address: String 
+    pub server_ip_address: String,
+    pub server_port: u16,
+    pub parani_asset_number: u16,
+    pub logging_type: String,
+    pub syslog_server_ip_address: String 
 }
 
 impl Configuration {
@@ -14,7 +15,8 @@ impl Configuration {
     
         Configuration {
             server_ip_address: config.get("networking", "server_ip_address").unwrap(),
-            server_port: config.getuint("networking", "server_port").unwrap().unwrap(),
+            server_port: config.get("networking", "server_port").unwrap().parse::<u16>().unwrap(),
+            parani_asset_number: config.get("parani", "asset_number").unwrap().parse::<u16>().unwrap(),
             logging_type: config.get("logging", "logging_type").unwrap(),
             syslog_server_ip_address: config.get("logging", "syslog_server_ip_address").unwrap()
         }
