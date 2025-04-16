8208f8ae8864a7f22e1c881488f2ec0862865013
yoxxa <theyoxxa@protonmail.com>
2025-04-16 21:00:47 +0800

    improvement: configured naming for each thread

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,26 @@ fn main() {
     let config_parani = config.clone();
     let config_uconnect = config.clone();
 
-    let heartbeat = thread::spawn( || {
+    let heartbeat = thread::Builder::new().name("heartbeat".to_string()).spawn( || {
         let heartbeat = networking::Heartbeat::new(config);
         loop {
             heartbeat.run();
         }
     });
-    let parani = thread::spawn( || {
+    let parani = thread::Builder::new().name("parani".to_string()).spawn( || {
         let mut parani = parani::ParaniSD1000::new(config_parani);
         loop {
             parani.run();
         }
     });
-    let uconnect = thread::spawn( || {
+    let uconnect = thread::Builder::new().name("uconnect".to_string()).spawn( || {
         let mut uconnect = uconnect::UConnectS2B5232R::new(config_uconnect);
         loop {
             uconnect.run();
         }
     });
 
-    heartbeat.join().unwrap();
-    parani.join().unwrap();
-    uconnect.join().unwrap();
+    heartbeat.unwrap().join().unwrap();
+    parani.unwrap().join().unwrap();
+    uconnect.unwrap().join().unwrap();
 }
