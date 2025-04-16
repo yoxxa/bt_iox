83cad4e8fef9df031d7e3339cbdfe12b97edb318
yoxxa <theyoxxa@protonmail.com>
2025-04-16 20:49:37 +0800

    feature: implemented second dongle UConnectS2B5232R struct and logic for gathering data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 
 mod parani;
+mod uconnect;
 mod networking;
 mod config;
 
@@ -8,10 +9,13 @@ fn main() {
     // TODO - use a single Configuration and pass to all threads?
     let config = config::Configuration::new();
     let config_parani = config.clone();
+    let config_uconnect = config.clone();
 
     let heartbeat = thread::spawn( || {
         let heartbeat = networking::Heartbeat::new(config);
-        heartbeat.run();
+        loop {
+            heartbeat.run();
+        }
     });
     let parani = thread::spawn( || {
         let mut parani = parani::ParaniSD1000::new(config_parani);
@@ -19,7 +23,14 @@ fn main() {
             parani.run();
         }
     });
+    let uconnect = thread::spawn( || {
+        let mut uconnect = uconnect::UConnectS2B5232R::new(config_uconnect);
+        loop {
+            uconnect.run();
+        }
+    });
 
     heartbeat.join().unwrap();
     parani.join().unwrap();
+    uconnect.join().unwrap();
 }
---
+++ src/uconnect.rs
@@ -0,0 +1,80 @@
+use std::io::{BufReader, BufRead};
+use std::time::Duration;
+use std::env;
+use std::net::UdpSocket;
+
+use time::OffsetDateTime;
+use serialport;
+
+use crate::{
+    networking::IncomingMessageProtocol,
+    config::Configuration
+};
+
+#[derive(Debug)]
+struct Data {
+    mac_address: String,
+    timestamp: OffsetDateTime
+}
+
+pub struct UConnectS2B5232R {
+    config: Configuration,
+    port: Box<dyn serialport::SerialPort>,
+    data: Option<Data>
+}
+
+impl UConnectS2B5232R {
+    pub fn new(config: Configuration) -> UConnectS2B5232R {
+        Self {
+            config: config,
+            port: serialport::new(
+                env::var("IR_OTHER_SERIAL").unwrap_or("/dev/ttySerial1".into()), 
+                115200
+                )
+                // TODO - figure out appropriate timeout
+                .timeout(Duration::from_secs(600))
+                .open().expect("Failed to open port"),
+            data: None
+        }
+    }
+
+    fn collect_data(&mut self) {
+        let mut port = BufReader::new(&mut *self.port);
+
+        let mut line = String::new();
+            // TODO - Handle timeout error, will occur once .timeout() duration is reached. Use match.
+        port.read_line(&mut line).expect("Error reading");
+    
+        // TODO - Use match statements in future iteration?
+        if line != "\r\n"
+        && line != "\\r\\n"
+        && line != "" {
+            let parts = line.split(",");
+            let collection: Vec<&str> = parts.collect();
+                    
+            self.data = Some(Data {
+                mac_address: collection[3].to_string(),
+                timestamp: OffsetDateTime::now_utc()
+            });
+        } 
+    }
+
+    fn send_data_to_server(&self, socket: &UdpSocket) {
+        let packet: IncomingMessageProtocol = IncomingMessageProtocol::new(
+            self.config.asset_number,
+            self.data.as_ref().unwrap().mac_address.as_bytes().try_into().unwrap(),
+            self.data.as_ref().unwrap().timestamp
+        );
+        packet.send_imp_v1(&socket);
+    }
+    
+    pub fn run(&mut self) {
+        let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+        socket.connect(format!("{}:{}", self.config.server_ip_address, self.config.server_port))
+            .expect("connect function failed");
+        loop {
+            self.collect_data();
+            self.send_data_to_server(&socket);
+        }
+    }
+}
