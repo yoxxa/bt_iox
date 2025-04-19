0eae71f24fa096009d5e875fd634eb5f9e50d2fc
yoxxa <theyoxxa@protonmail.com>
2025-04-19 11:29:14 +0800

    improvement: logging in main.rs, reporting Configuration initialisation, and thread starts

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 use std::thread;
+use tracing::{info, instrument};
+use tracing_subscriber;
 
 mod parani;
 mod uconnect;
 mod networking;
 mod config;
 
+#[instrument]
 fn main() {
+    tracing_subscriber::fmt::init();
+    info!("Initialised tracing_subscriber");
+    
     // TODO - use a single Configuration and pass to all threads?
     let config = config::Configuration::new();
     let config_parani = config.clone();
     let config_uconnect = config.clone();
+    info!("Gathered configuration");
 
     let heartbeat = thread::Builder::new().name("heartbeat".to_string()).spawn( || {
+        info!("Started heartbeat thread");
         let heartbeat = networking::Heartbeat::new(config);
         heartbeat.run();
     });
     let parani = thread::Builder::new().name("parani".to_string()).spawn( || {
+        info!("Started parani thread");
         let mut parani = parani::ParaniSD1000::new(config_parani);
         parani.run();
     });
     let uconnect = thread::Builder::new().name("uconnect".to_string()).spawn( || {
+        info!("Started uconnect thread");
         let mut uconnect = uconnect::UConnectS2B5232R::new(config_uconnect);
         uconnect.run();
     });
