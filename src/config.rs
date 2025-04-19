131bf5ed358cd5f0f9f71e2d49ddd7961dec36f1
yoxxa <theyoxxa@protonmail.com>
2025-04-19 11:42:26 +0800

    improvement: logging for config.rs, reporting match case result for .load()

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::hash::RandomState;
-
+use tracing::{instrument, error, debug};
 use configparser::ini::Ini;
 
 #[derive(Clone)]
@@ -11,13 +11,20 @@ pub struct Configuration {
 }
 
 impl Configuration {
+    #[instrument]
     pub fn new() -> Configuration {
         let mut config = Ini::new();
         match config.load("/iox_data/package_config.ini") {
-            Ok(config) => config,
-            Err(_) => { panic!("Failed to load package_config.ini, check if fields are valid"); }
+            Ok(config) => {
+                debug!("Ok() received for .load() on type: Ini");
+                config
+            },
+            Err(error) => { 
+                error!("Err() received for .load() on type: Ini - {error}");
+                panic!("Failed to load package_config.ini, check if fields are valid");
+            }
         };
-    
+        // TODO - implement checking for these values instead of implicit .unwrap()
         Configuration {
             server_ip_address: config.get("networking", "server_ip_address").unwrap(),
             server_port: config.get("networking", "server_port").unwrap().parse::<u16>().unwrap(),
